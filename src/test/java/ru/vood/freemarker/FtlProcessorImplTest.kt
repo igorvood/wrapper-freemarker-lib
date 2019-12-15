45d3a73792790df1af3a69ecedc8fe502342be0d
vood <balitsky_igor@list.ru>
2019-12-15 18:29:12 +0300

    tests

--- src/test/java/ru/vood/freemarker/FtlProcessorImplTest.kt
+++ src/test/java/ru/vood/freemarker/FtlProcessorImplTest.kt
@@ -35,17 +35,18 @@ internal class FtlProcessorImplTest {
 
     @Test
     fun processFileByClassWithParamObject_1() {
-        val stringParam = Car("BMW", 1234, Date(2000, 3, 13))
+        val stringParam = Car("BMW", 1234, Date())
         val processFile = ftlProcessor.processFile(FtlProcessorImplTest::class.java, "FtlProcessorImplTestWithParam.ftl", arrayOf(stringParam))
         Assertions.assertEquals("PARAM->$stringParam", processFile)
     }
 
     @Test
     fun processFileByClassWithParamObject_2() {
-        val stringParam = Car("BMW", 123456789, Date())
+        val stringParam = Car("BMW", 123456789, Date(100, 3, 13))
         val processFile = ftlProcessor.processFile(FtlProcessorImplTest::class.java, "FtlProcessorImplTestWithParamObject.ftl", arrayOf(stringParam))
         Assertions.assertTrue(processFile.contains("model->BMW,price->123 456 789,date->"))
         Assertions.assertTrue(processFile.contains(",dateTime->"))
+        Assertions.assertEquals("model->BMW,price->123 456 789,date->13.04.2000,dateTime->13.04.2000 0:00:00", processFile)
     }
 
     @Test
