3ca27079294ea9752b479113bfc70e80c78d202f
vood <balitsky_igor@list.ru>
2020-01-03 12:33:39 +0300

    ext free

--- src/main/java/ru/vood/freemarker/FtlProcessor.kt
+++ src/main/java/ru/vood/freemarker/FtlProcessor.kt
@@ -6,11 +6,11 @@ import java.util.*
 interface FtlProcessor {
 
     //    fun processFile(fileName: String, args: Array<Any?>? = null): String
-    fun processFile(file: File, args: Array<Any?>? = null): String
+    fun processFile(file: File, args: Array<Any>? = null): String
 
-    fun processFile(fileName: String, args: Array<Any?>?) = processFile(resolveFile(fileName), args)
+    fun processFile(fileName: String, args: Array<Any>?) = processFile(resolveFile(fileName), args)
 
-    fun processFile(clazz: Class<*>, fileName: String, args: Array<Any?>?) = processFile("""${clazz.name.replace(".", "/")}/$fileName""", args)
+    fun processFile(clazz: Class<*>, fileName: String, args: Array<Any>?) = processFile("""${clazz.name.replace(".", "/")}/$fileName""", args)
 
     private fun resolveFile(fileName: String): File {
         val resource = javaClass.classLoader.getResource(fileName)
